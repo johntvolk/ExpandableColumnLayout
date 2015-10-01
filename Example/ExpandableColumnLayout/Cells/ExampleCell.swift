3540b8e1c9de13033c35e08181e1c79022a9ae07
John Volk <john.volk@novu.com>
2015-10-01 10:56:38 -0500

    Updating some copyrights.

--- Example/ExpandableColumnLayout/Cells/ExampleCell.swift
+++ Example/ExpandableColumnLayout/Cells/ExampleCell.swift
@@ -3,7 +3,7 @@
 //  ExpandableColumnLayout
 //
 //  Created by John Volk on 7/21/15.
-//  Copyright (c) 2015 CocoaPods. All rights reserved.
+//  Copyright (c) 2015 John Volk. All rights reserved.
 //
 
 import UIKit
